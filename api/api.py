d0ef6ef5b2a1bff862bf9f68d5ff3cab5c7deacd
lilJuan4168 <chesteraraoz12.0@gmail.com>
2024-01-23 23:14:44 -0300

    updates

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
 from api.functions import *
 import os
 
@@ -8,6 +9,11 @@ app = FastAPI()
 async def get_ip():
             x = ip()
             return x
+
+@app.get("/gen_key")
+async def gen_key(my_pwd):
+    my_key = create_key(my_pwd)
+    return 
 
 @app.post("/sharing/upload")
 async def sharing_upload_file(file: UploadFile):
@@ -23,13 +29,9 @@ async def sharing_upload_file(file: UploadFile):
 
 
 @app.post("/encrypt/upload")
-async def sharing_upload_file(file: UploadFile):
+async def sharing_upload_file(key, file: UploadFile):
     try:
-        os.makedirs("files", exist_ok=True)
-        file_path = os.path.join("files", file.filename)
-
-        with open(file_path, "wb") as buffer:
-            buffer.write(file.file.read())
-        return {"status": 200,"filename": file.filename}
+        data = data_encrypt(key, file)
+        return FileResponse(media_type="multipart/form-data", filename=data)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error, file not saved: {str(e)}")
