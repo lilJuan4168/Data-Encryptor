bc8b89427a03826617c434f4f179c6b3f6308bcb
lilJuan4168 <chesteraraoz12.0@gmail.com>
2024-01-28 01:27:42 -0300

    some updates

--- cli/decrypter_cli.py
+++ cli/decrypter_cli.py
@@ -3,24 +3,26 @@ from tqdm import tqdm
 
 wh_status = True
 while wh_status == True:
-    print("\n---Data Encrypter---\n")
+    print("\n---Data Decrypter---\n")
     path = str(input("Write your path: "))
     pwd = str(input("Write your password: "))
     os.system("clear")
     if len(path) != 0 and len(pwd) != 0:
-        key = create_key(pwd, path)
+        file = open(f"{path}/password.pickle", "rb")
+        key = pickle.load(file)
+        file.close()
         files = os.listdir(path)
         files.remove("password.pickle")
         n_files = len(files)
-        print(f"You are about to encrypt {n_files}")
+        print(f"You are about to decrypt {n_files}")
         feedback = input("Continue? [Y/n]: ")
         if feedback.upper() == "Y":
             for file in tqdm(files):
                 vid_path = path+"/"+file
-                data_encrypt(key, path, file, pwd)
+                data_decrypt(key, path, file, pwd)
                 sleep(1)
             os.system("clear")
-            print("Completed, Save your password.pickle file and do not forget your password")
+            print("Completed")
             input("Press enter to exit")
             wh_status = False
             break
---
+++ cli/functions_cli.py
@@ -0,0 +1,35 @@
+from cryptography.fernet import Fernet
+import pickle
+import os
+from datetime import datetime
+from time import sleep
+
+def create_key(my_password, path):
+    key = Fernet.generate_key()
+    file_key = open(f"{path}/password.pickle", "wb")
+    pickle.dump(key, file_key)
+    file_key.close()
+    return key
+
+def data_encrypt(key, path, file, pwd):
+    my_key = Fernet(key + pwd.encode('utf-8'))
+    with open(f"{path}/{file}", "rb") as v:
+        video = v.read()
+    video_encrypted = my_key.encrypt(video)
+    file_name = str(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+    os.mkdir(f"{path}/encrypted_files")
+    pkfile = open(f"{path}/encrypted_files/{file_name}.pickle", "wb")
+    pickle.dump(video_encrypted, pkfile)
+    pkfile.close()
+    return True
+
+def data_decrypt(key, path, file, pwd):
+    my_key = Fernet(key + pwd.encode('utf-8'))
+    filepk = open(f"{path}/{file}", "rb")
+    videopk = pickle.load(filepk)
+    filepk.close()
+    video_decrypted = my_key.decrypt(videopk)
+    os.mkdir(f"{path}/decrypted_files")
+    with open(f"{path}/decrypted_files/{file}.mp4", "wb") as dc:
+        dc.write(video_decrypted)
+    return video_decrypted
